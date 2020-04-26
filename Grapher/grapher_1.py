561a94134811f437b6a712b4c9182b11dd716c23
Adam Johnson <johnsonadam187@gmail.com>
2020-04-26 22:52:45 +1200

    Initial commit

---
+++ Grapher/grapher_1.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# 1 determine what type of data
+# 2 determine which plot
+# 3 determine layout
+
+
+class Chart:
+
+    def __init__(self):
+        pass
