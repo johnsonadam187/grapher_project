8a5baef8649736ae76fec317a8ee6723b53d2ea3
Adam Johnson <johnsonadam187@gmail.com>
2020-04-27 21:47:41 +1200

    Added header and index col cleaning functions for correct importation of database for charting

--- Chart_gen/chart_gen.py
+++ Chart_gen/chart_gen.py
@@ -5,19 +5,35 @@ import seaborn as sns
 
 
 def new_chart():
-    pass
+    """Creates as dict for storage of chart objects and attributes"""
+    return {}
 
-def set_title(object, title):
-    pass
 
-def set_x_axis(object, title):
-    pass
+def set_main_title(object, title):
+    """Creates ability to save the proposed title as 'main_title' in the new_chart dict"""
+    object['main_title'] = title
 
-def set_y_axis(object, title):
-    pass
 
-def check_data(object, series):
-    pass
+def set_x_title(object, title):
+    """Creates ability to save the proposed label as 'x_title' in the new_chart dict"""
+    object['x_title'] = title
+
+
+def set_y_title(object, title):
+    """Creates ability to save the proposed label as 'y_title' in the new_chart dict"""
+    object['y_title'] = title
+
+
+def data_type(object, title):
+    """Creates ability to specify the proposed data type as 'data_type' in the new_chart dict"""
+    object['data_type'] = title
+
 
 def use_data(object, series):
-    pass
+    """Creates ability to specify the proposed data as 'data' in the new_chart dict"""
+    object['data'] = series
+
+
+def chart_type(object, chart_style):
+    "Creates ability for chart style to be stored in dict as 'chart_style"
+    object['chart_style'] = chart_style
--- Chart_gen/generator.py
+++ Chart_gen/generator.py
@@ -5,7 +5,7 @@ import seaborn as sns
 from PyQt5.QtWidgets import QFileDialog, QApplication
 import sys
 
-# TODO create ability for dialog file open,
+# TODO further add functions for data clean and series selection
 #  check if data is list, series or other,
 #  create defaults for each plotting function,
 
@@ -26,6 +26,76 @@ def file_check(filename):
     elif file_ext == "csv":
         df1 = pd.read_csv(filename)
         return df1
+    elif file_ext == 'html':
+        df1 = pd.read_html(filename)
+        return df1
+    else:
+        df1 = pd.read_html(filename)
+        return df1
+
+
+def fix_header(dataframe):
+    """Function for data cleaning, allows for manual specification of the header row and replaces, returning
+    resultant dataframe"""
+    loop = True
+    while loop:
+        fix = input('Change Header Row? Type Y/N').upper()
+        if fix == "Y":
+            loop2 = True
+            while loop2:
+                if loop2:
+                    print(dataframe.to_string())
+                    header_row = int(input("Specify Header Row (from 0)"))
+                    headers = dataframe.iloc[header_row]
+                    new_df = dataframe[header_row:]
+                    new_df.columns = headers
+                    print(new_df.to_string())
+                    correct = input("Is this correct? 'Y/N'").upper()
+                    if correct == "Y":
+                        loop2 = False
+                        return new_df
+                    elif correct == "N":
+                        continue
+                    else:
+                        print("Incorrect specification, try again.")
+            loop = False
+        elif fix == "N":
+            loop = False
+            return dataframe
+        else:
+            print("Incorrect entry, enter 'Y' or 'N'.")
+            continue
+
+
+def fix_index(dataframe):
+    """Function for data cleaning, allows for manual specification of the index column and replaces, returning
+    resultant dataframe"""
+    loop = True
+    while loop:
+        fix = input('Change Index Column? Type Y/N').upper()
+        if fix == "Y":
+            loop2 = True
+            while loop2:
+                if loop2:
+                    print(dataframe.columns)
+                    index_col = [(input("Specify Index Column (by name)"))]
+                    dataframe.set_index(index_col, inplace=True)
+                    print(dataframe.to_string())
+                    correct = input("Is this correct? 'Y/N'").upper()
+                    if correct == "Y":
+                        loop2 = False
+                        return dataframe
+                    elif correct == "N":
+                        continue
+                    else:
+                        print("Incorrect specification, try again.")
+            loop = False
+        elif fix == "N":
+            loop = False
+        else:
+            print("Incorrect entry, enter 'Y' or 'N'.")
+            continue
+
     # # try:
     #     with open(filename, 'r') as f:
     #         data = f.read()
@@ -46,5 +116,6 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     file_name = file_open()
     data = file_check(file_name)
-    print(data)
+    new_df = fix_header(data)
+    new_df2 = fix_index(new_df)
     sys.exit(app.exec_())
